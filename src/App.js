6b41f958d57c2b770e89ebb1e26186adb44dc1fa
Joel <joelcjohnson123@gmail.com>
2019-05-29 12:32:21 +0530

    Updated the tab

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+import 'materialize-css/dist/css/materialize.min.css';
 
 import Grid from '@material-ui/core/Grid';
 import './components/css/style.css';
@@ -8,136 +9,184 @@ import Header from './components/Header';
 import MenuBar from './components/MenuBar';
 
 const styles = theme => ({
-  root: {
-    flexGrow: 1,
-  },
-  body:{
-    backgroundColor:'black'
-  },
-  h1:{
-    padding: '10px 0px 20px 0px',
-    margin: '0px',
-    textAlign:'center',
-    color:'#fff',
-    backgroundColor:'black'
-
-  },
-  paper: {
-    padding: theme.spacing.unit * 2,
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
-  textDisplay:{
-    padding: theme.spacing.unit,
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-    marginBottom:'10px',
-    height: 140,
-    overflow:'auto',
-    textAlign:'justify',
-    lineHeight:'25px'
-  },
-  tokenList:{
-    // padding: theme.spacing.unit * 2,
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-    // marginBottom:'20px',
-    height: 320,
-    overflowX:'hidden',
-    overflowY:'auto',
-    backgroundColor:'#fff',
-  },
-  selectButtonPaper:{
-    padding: theme.spacing.unit,
-    // padding: '2px',
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-    marginLeft: '0px',
-    marginTop:'0%',
-    marginBottom:'10px'
-
-  },
-  spanning:{
-    color:'blue'
-  },
-  mainGrid:{
-    // backgroundColor:"blue",
-    width:'100%',
-    // marginLeft:'auto',
-    // marginRight:'auto',
-    // backgroundColor:'blue',
-    // paddingLeft:'20px',
-    // paddingRight:'20px',
-    paddingTop:'0',
-    marginTop:'0',
-    height:'auto',
-    // backgroundImage: `url(${Image})`,
-    // backgroundSize: 'cover',
-    // backgroundRepeat: 'no-repeat'
-  },
-  selectTwo:{
-    padding: theme.spacing.unit,
-    // padding: '2px',
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-    marginLeft: '0px',
-    position:'fixed',
-    // top:'20%',
-    // marginBottom:'10%',
-    width:'29%',
-
-  },
-  inputLabel:{
-    padding:'2px',
-    width:'20px',
-    marginBottom:'10px',
-    marginRight:'10%',
-    backgroundColor:'blue'
-  },
-  ul:{
-    marginRight:'-10%',
-    marginTop:'20%',
-  },
-  li:{
-    width:'100%',
-    float:'left',
-    listStyle:'none',
-    border:'1px solid grey',
-    padding:'5px'
-  },
-  ulDiv:{
-    overflow:'auto',
-    height:100,
-    borderBottom:'1px solid black'
-  },
-  selectMenu:{
-    width:'120px',
-    // backgroundColor:'blue'
-  },
-  highlightToken:{
-    color:'blue',
-    backgroundColor:'yellow'
-  }
+	root: {
+		flexGrow: 1,
+	},
+	body: {
+		backgroundColor: 'black',
+	},
+	h1: {
+		padding: '10px 0px 20px 0px',
+		margin: '0px',
+		textAlign: 'center',
+		color: '#fff',
+		backgroundColor: 'black',
+	},
+	paper: {
+		padding: theme.spacing.unit * 2,
+		textAlign: 'center',
+		color: theme.palette.text.secondary,
+	},
+	textDisplay: {
+		padding: theme.spacing.unit,
+		textAlign: 'center',
+		color: theme.palette.text.secondary,
+		marginBottom: '10px',
+		height: 140,
+		overflow: 'auto',
+		textAlign: 'justify',
+		lineHeight: '25px',
+	},
+	tokenList: {
+		// padding: theme.spacing.unit * 2,
+		textAlign: 'center',
+		color: theme.palette.text.secondary,
+		// marginBottom:'20px',
+		height: 320,
+		overflowX: 'hidden',
+		overflowY: 'auto',
+		backgroundColor: '#fff',
+	},
+	selectButtonPaper: {
+		padding: theme.spacing.unit,
+		// padding: '2px',
+		textAlign: 'center',
+		color: theme.palette.text.secondary,
+		marginLeft: '0px',
+		marginTop: '0%',
+		marginBottom: '10px',
+	},
+	spanning: {
+		color: 'blue',
+	},
+	mainGrid: {
+		// backgroundColor:"blue",
+		width: '100%',
+		// marginLeft:'auto',
+		// marginRight:'auto',
+		// backgroundColor:'blue',
+		// paddingLeft:'20px',
+		// paddingRight:'20px',
+		paddingTop: '0',
+		marginTop: '0',
+		height: 'auto',
+		// backgroundImage: `url(${Image})`,
+		// backgroundSize: 'cover',
+		// backgroundRepeat: 'no-repeat'
+	},
+	selectTwo: {
+		padding: theme.spacing.unit,
+		// padding: '2px',
+		textAlign: 'center',
+		color: theme.palette.text.secondary,
+		marginLeft: '0px',
+		position: 'fixed',
+		// top:'20%',
+		// marginBottom:'10%',
+		width: '29%',
+	},
+	inputLabel: {
+		padding: '2px',
+		width: '20px',
+		marginBottom: '10px',
+		marginRight: '10%',
+		backgroundColor: 'blue',
+	},
+	ul: {
+		marginRight: '-10%',
+		marginTop: '20%',
+	},
+	li: {
+		width: '100%',
+		float: 'left',
+		listStyle: 'none',
+		border: '1px solid grey',
+		padding: '5px',
+	},
+	ulDiv: {
+		overflow: 'auto',
+		height: 100,
+		borderBottom: '1px solid black',
+	},
+	selectMenu: {
+		width: '120px',
+		// backgroundColor:'blue'
+	},
+	highlightToken: {
+		color: 'blue',
+		backgroundColor: 'yellow',
+	},
 });
 
 function App(props) {
-  // console.log(props)
-  const { classes } = props;
-  // console.log(classes)
-  return (
-    // <div className="mainGrid9">
-      <Grid className={classes.mainGrid}>
-        <Grid item xs={12}>
-          <Header classes={classes}/>
-        </Grid>
-        <MenuBar classes={classes} />
-      </Grid>
-    // </div>
-  );
+	// console.log(props)
+	const { classes } = props;
+	// console.log(classes)
+	return (
+		// <div className="mainGrid9">
+		// <Grid className={classes.mainGrid}>
+		//   <Grid item xs={12}>
+		//     <Header classes={classes}/>
+		//   </Grid>
+		//   <MenuBar classes={classes} />
+		// </Grid>
+		<div>
+			<nav className="nav-extended">
+				<div className="nav-wrapper">
+					<a href="#" className="brand-logo">
+						AGMT
+					</a>
+					<a href="#" data-activates="mobile-demo" className="button-collapse">
+						<i className="material-icons">menu</i>
+					</a>
+					<ul id="nav-mobile" className="right hide-on-med-and-down">
+						<li>
+							<a href="#">About</a>
+						</li>
+						<li>
+							<a href="#">Contact Us</a>
+						</li>
+						<li>
+							<a href="Logout">Logout</a>
+						</li>
+					</ul>
+					{/* <ul className="side-nav" id="mobile-demo">
+							<li className="active">
+								<a href="https://www.jquery-az.com/jquery-tips/">jQuery</a>
+							</li>
+							
+						</ul> */}
+				</div>
+				<div className="nav-content container">
+					<ul className="tabs center tabs-transparent">
+						<li className="tab">
+							<a href="#tab1">Available Source</a>
+						</li>
+						<li className="tab">
+							<a href="#tab2">Translation</a>
+						</li>
+						<li className="tab">
+							<a href="#tab3">Download Draft</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+			<div id="tab1" className="col s12">
+				Content for Tab 1
+			</div>
+			<div id="tab2" className="col s12">
+				<MenuBar classes={classes} />
+			</div>
+			<div id="tab3" className="col s12">
+				Content for Tab 3
+			</div>
+		</div>
+		// </div>
+	);
 }
 
 App.propTypes = {
-  classes: PropTypes.object.isRequired,
+	classes: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles)(App);
--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,179 +1,216 @@
 import React, { Component } from 'react';
 import { FormControl, Grid, Paper, MenuItem, Select, InputLabel } from '@material-ui/core';
 import Translations from './Translations';
-import './css/style.css'
+import './css/style.css';
 
 class MenuBar extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            languageVersionData: {},
-            language: '',
-            version: '',
-            bookList: '',
-            book: '',
-            tokenList: [],
-            usfmTextFlag: false
-        }
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			languageVersionData: {},
+			language: '',
+			version: '',
+			bookList: '',
+			book: '',
+			tokenList: [],
+			usfmTextFlag: false,
+		};
+	}
 
-    getLanguageData = async () => {
-        const result = await fetch("http://127.0.0.1:8000/v1/getlanguages", {
-            method: 'GET'
-        });
-        const mJson = await result.json();
-        var languageVersionData = mJson.languages
-        this.setState({ languageVersionData })
-    }
+	getLanguageData = async () => {
+		const result = await fetch('http://127.0.0.1:8000/v1/getlanguages', {
+			method: 'GET',
+		});
+		const mJson = await result.json();
+		var languageVersionData = mJson.languages;
+		this.setState({ languageVersionData });
+		// console.log(languageVersionData)
+	};
 
+	async getBooks() {
+		var book = await fetch('http://127.0.0.1:8000/v1/books/' + this.state.language + '/' + this.state.version, {
+			method: 'GET',
+		});
+		const mJson = await book.json();
+		this.setState({
+			bookList: mJson,
+		});
+	}
 
-    async getBooks() {
-        var book = await fetch('http://127.0.0.1:8000/v1/books/' + this.state.language + '/' + this.state.version, {
-            method: 'GET'
-        })
-        const mJson = await book.json();
-        this.setState({
-            bookList: mJson
-        })
+	getLanguages(value) {
+		var languages = Object.keys(value);
+		return languages.map(item => {
+			return (
+				<MenuItem key={item} value={item}>
+					{item}
+				</MenuItem>
+			);
+		});
+	}
 
-    }
+	getVersions(language) {
+		if (!language) {
+			return (
+				<MenuItem key="" value="">
+					Loading Versions
+				</MenuItem>
+			);
+		}
+		return this.state.languageVersionData[language].map(item => {
+			return (
+				<MenuItem key={item.id} value={item.version}>
+					{item.version}
+				</MenuItem>
+			);
+		});
+	}
 
-    getLanguages(value) {
-        var languages = Object.keys(value);
-        return languages.map(item => {
-            return (
-                <MenuItem key={item} value={item}>{item}</MenuItem>
-            )
-        })
-    }
+	async componentDidMount() {
+		this.getLanguageData();
+	}
 
-    getVersions(language) {
-        if (!language) {
-            return <MenuItem key="" value="">Loading Versions</MenuItem>
-        }
-        return this.state.languageVersionData[language].map(item => {
-            return (
-                <MenuItem key={item.id} value={item.version}>{item.version}</MenuItem>
-            )
-        })
-    }
+	async getTokenList() {
+		var book = await fetch(
+			'http://127.0.0.1:8000/v1/tokenlist/' +
+				this.state.language +
+				'/' +
+				this.state.version +
+				'/' +
+				this.state.book,
+			{
+				method: 'GET',
+			}
+		);
+		const tokenList = await book.json();
 
-    async componentDidMount() {
-        this.getLanguageData()
-    }
+		// var getUsfm = await fetch('http://127.0.0.1:8000/v1/usfmtexts/' + this.state.language + '/' + this.state.version + '/' + this.state.book, {
+		//     method: 'GET'
+		// })
+		// const usfmJson = await getUsfm.json();
+		// const usfmText = usfmJson[this.state.book]
+		this.setState({
+			tokenList,
+			// usfmText
+		});
+	}
+	getBookItems() {
+		// this.getBooks()
+		if (this.state.bookList) {
+			return this.state.bookList.map(item => {
+				return (
+					<MenuItem key={item} value={item}>
+						{item}
+					</MenuItem>
+				);
+			});
+		} else {
+			return (
+				<MenuItem key="" value="">
+					Select Book
+				</MenuItem>
+			);
+		}
+	}
 
-    async getTokenList() {
-        var book = await fetch('http://127.0.0.1:8000/v1/tokenlist/' + this.state.language + '/' + this.state.version + '/' + this.state.book, {
-            method: 'GET'
-        })
-        const tokenList = await book.json();
-
-        // var getUsfm = await fetch('http://127.0.0.1:8000/v1/usfmtexts/' + this.state.language + '/' + this.state.version + '/' + this.state.book, {
-        //     method: 'GET'
-        // })
-        // const usfmJson = await getUsfm.json();
-        // const usfmText = usfmJson[this.state.book]
-        this.setState({
-            tokenList,
-            // usfmText
-        })
-    }
-    getBookItems() {
-        // this.getBooks()
-        if (this.state.bookList) {
-            return this.state.bookList.map(item => {
-                return (
-                    <MenuItem key={item} value={item}>{item}</MenuItem>
-                )
-            })
-        } else {
-            return (
-                <MenuItem key="" value="">Select Book</MenuItem>
-            )
-        }
-    }
-
-    render() {
-        return (
-            <Grid className={this.props.classes.mainGrid} container spacing={24}>
-                <Grid item xs={4} md={2}>
-                
-                    <Paper className={this.props.classes.selectButtonPaper}>
-                    <FormControl className={this.props.classes.formControl}>
-                        <InputLabel htmlFor="select-language">Language</InputLabel><br/>
-                        <Select
-                            className={this.props.classes.selectMenu}
-                            // id="formatted-text-mask-input"
-                            value={this.state.language}
-                            onChange={(e) => this.setState({ 
-                                language: e.target.value, 
-                                version: '', 
-                                book:'',
-                                usfmTextFlag: false
-                            })}
-                            inputProps={{
-                                id: 'select-language',
-                            }}
-                        >
-                            {this.getLanguages(this.state.languageVersionData)}
-                        </Select>
-                        </FormControl>
-                    </Paper>
-                </Grid>
-                <Grid item xs={4} md={2}>
-                    <Paper className={this.props.classes.selectButtonPaper}>
-                    <FormControl className={this.props.classes.formControl}>
-                        <InputLabel htmlFor="select-version">Version</InputLabel><br/>
-                        <Select
-                            className={this.props.classes.selectMenu}
-                            value={this.state.version}
-                            onChange={(e) => this.setState({ 
-                                version: e.target.value, 
-                                book:'',
-                                usfmTextFlag: false
-                             }, () => { this.getBooks() })}
-                            inputProps={{
-                                id: 'select-version',
-                            }}
-                        >
-                            {this.getVersions(this.state.language)}
-                        </Select>
-                        </FormControl>
-                    </Paper>
-                </Grid>
-                <Grid item xs={4} md={2}>
-                    <Paper className={this.props.classes.selectButtonPaper}>
-                    <FormControl className={this.props.classes.formControl}>
-                        <InputLabel htmlFor="select-book">Book</InputLabel><br/>
-                        <Select
-                            className={this.props.classes.selectMenu}
-                            value={this.state.book}
-                            onChange={(e) => this.setState({ 
-                                book: e.target.value, 
-                                usfmTextFlag: false 
-                            }, () => { this.getTokenList() })}
-                            inputProps={{
-                                id: 'select-book',
-                            }}
-                        >
-                            {this.getBookItems()}
-
-                        </Select>
-                        </FormControl>
-                    </Paper>
-                </Grid>   
-                <Translations data={{
-                    classes: this.props.classes,
-                    book: this.state.book,
-                    language: this.state.language,
-                    version: this.state.version,
-                    tokenList: this.state.tokenList,
-                    usfmTextFlag: this.state.usfmTextFlag
-                }} />
-            </Grid>
-        )
-    }
+	render() {
+		return (
+			<Grid className={this.props.classes.mainGrid} container spacing={24}>
+				<Grid item xs={4} md={2}>
+					<Paper className={this.props.classes.selectButtonPaper}>
+						<FormControl className={this.props.classes.formControl}>
+							<InputLabel htmlFor="select-language">Language</InputLabel>
+							<br />
+							<Select
+								className={this.props.classes.selectMenu}
+								// id="formatted-text-mask-input"
+								value={this.state.language}
+								onChange={e =>
+									this.setState({
+										language: e.target.value,
+										version: '',
+										book: '',
+										usfmTextFlag: false,
+									})
+								}
+								inputProps={{
+									id: 'select-language',
+								}}
+							>
+								{this.getLanguages(this.state.languageVersionData)}
+							</Select>
+						</FormControl>
+					</Paper>
+				</Grid>
+				<Grid item xs={4} md={2}>
+					<Paper className={this.props.classes.selectButtonPaper}>
+						<FormControl className={this.props.classes.formControl}>
+							<InputLabel htmlFor="select-version">Version</InputLabel>
+							<br />
+							<Select
+								className={this.props.classes.selectMenu}
+								value={this.state.version}
+								onChange={e =>
+									this.setState(
+										{
+											version: e.target.value,
+											book: '',
+											usfmTextFlag: false,
+										},
+										() => {
+											this.getBooks();
+										}
+									)
+								}
+								inputProps={{
+									id: 'select-version',
+								}}
+							>
+								{this.getVersions(this.state.language)}
+							</Select>
+						</FormControl>
+					</Paper>
+				</Grid>
+				<Grid item xs={4} md={2}>
+					<Paper className={this.props.classes.selectButtonPaper}>
+						<FormControl className={this.props.classes.formControl}>
+							<InputLabel htmlFor="select-book">Book</InputLabel>
+							<br />
+							<Select
+								className={this.props.classes.selectMenu}
+								value={this.state.book}
+								onChange={e =>
+									this.setState(
+										{
+											book: e.target.value,
+											usfmTextFlag: false,
+										},
+										() => {
+											this.getTokenList();
+										}
+									)
+								}
+								inputProps={{
+									id: 'select-book',
+								}}
+							>
+								{this.getBookItems()}
+							</Select>
+						</FormControl>
+					</Paper>
+				</Grid>
+				<Translations
+					data={{
+						classes: this.props.classes,
+						book: this.state.book,
+						language: this.state.language,
+						version: this.state.version,
+						tokenList: this.state.tokenList,
+						usfmTextFlag: this.state.usfmTextFlag,
+					}}
+				/>
+			</Grid>
+		);
+	}
 }
-
 
 export default MenuBar;
